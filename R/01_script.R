f36de34e93dcaaa3d0a534a0a6ef887ee71e6555
mruizros <mruizros@gmail.com>
2021-08-10 13:28:56 -0500

    Primer script

---
+++ R/01_script.R
@@ -0,0 +1,7 @@
+library('MASS')
+data(MASS::cats)
+pdf("mis_imagenes/cats_plot.pdf") 
+ggplot(cats, aes(x = Sex)) + 
+  geom_bar(fill = "orange", color = "black") + theme_classic() +
+  xlab("Sexo") + ylab("Número de Gatos") + ggtitle("Gatos")
+dev.off()
